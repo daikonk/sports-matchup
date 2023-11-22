4aca93f5526a56b1c485946f7bd89ed86f3995e0
Samuel Kull <sam.kull9999@gmail.com>
2023-11-22 15:42:39 -0800

    Improvements in Create/Edit page.

--- sports-matchup/src/CreateEdit.js
+++ sports-matchup/src/CreateEdit.js
@@ -1,10 +1,36 @@
 // CreateEdit.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useState} from 'react';
 
 const CreateEdit = () => {
+    const [expertiseLevel, setExpertiseLevel] = useState('Beginner');
+    const [peopleCount, setPeopleCount] = useState(1);
+    const [selectedState, setSelectedState] = useState('');
+
+    const handleRangeChange = (event) => {
+        const value = parseInt(event.target.value);
+        let level = 'Beginner';
+        if (value === 1) {
+          level = 'Intermediate';
+        } else if (value === 2) {
+          level = 'Advanced';
+        } else if (value === 3) {
+          level = 'Expert';
+        }
+        setExpertiseLevel(level);
+    };
+
+    const handlePeopleCountChange = (event) => {
+        const newCount = Math.max(1, Math.min(10, Number(event.target.value)));
+        setPeopleCount(newCount);
+    };
+
+    const handleStateChange = (event) => {
+        setSelectedState(event.target.value);
+    };
+
     useEffect(() => {
         document.title = 'Create/Edit - Sports Matchup';
-      }, []);
+    }, []);
 
     return (
         <main>
@@ -51,20 +77,69 @@ const CreateEdit = () => {
                         <input type="text" className="form-control" id="inputCity"/>
                         </div>
                         <div className="col-md-4">
-                        <label for="inputState" className="form-label">State</label>
-                        <select id="inputState" className="form-select">
-                            <option selected>Choose...</option>
-                            <option>...</option>
-                        </select>
+                            <label htmlFor="inputState" className="form-label">State</label>
+                            <select id="inputState" className="form-select" value={selectedState} onChange={handleStateChange}>
+                                <option value="">Choose...</option>
+                                <option value="AL">Alabama</option>
+                                <option value="AK">Alaska</option>
+                                <option value="AZ">Arizona</option>
+                                <option value="AR">Arkansas</option>
+                                <option value="CA">California</option>
+                                <option value="CO">Colorado</option>
+                                <option value="CT">Connecticut</option>
+                                <option value="DE">Delaware</option>
+                                <option value="FL">Florida</option>
+                                <option value="GA">Georgia</option>
+                                <option value="HI">Hawaii</option>
+                                <option value="ID">Idaho</option>
+                                <option value="IL">Illinois</option>
+                                <option value="IN">Indiana</option>
+                                <option value="IA">Iowa</option>
+                                <option value="KS">Kansas</option>
+                                <option value="KY">Kentucky</option>
+                                <option value="LA">Louisiana</option>
+                                <option value="ME">Maine</option>
+                                <option value="MD">Maryland</option>
+                                <option value="MA">Massachusetts</option>
+                                <option value="MI">Michigan</option>
+                                <option value="MN">Minnesota</option>
+                                <option value="MS">Mississippi</option>
+                                <option value="MO">Missouri</option>
+                                <option value="MT">Montana</option>
+                                <option value="NE">Nebraska</option>
+                                <option value="NV">Nevada</option>
+                                <option value="NH">New Hampshire</option>
+                                <option value="NJ">New Jersey</option>
+                                <option value="NM">New Mexico</option>
+                                <option value="NY">New York</option>
+                                <option value="NC">North Carolina</option>
+                                <option value="ND">North Dakota</option>
+                                <option value="OH">Ohio</option>
+                                <option value="OK">Oklahoma</option>
+                                <option value="OR">Oregon</option>
+                                <option value="PA">Pennsylvania</option>
+                                <option value="RI">Rhode Island</option>
+                                <option value="SC">South Carolina</option>
+                                <option value="SD">South Dakota</option>
+                                <option value="TN">Tennessee</option>
+                                <option value="TX">Texas</option>
+                                <option value="UT">Utah</option>
+                                <option value="VT">Vermont</option>
+                                <option value="VA">Virginia</option>
+                                <option value="WA">Washington</option>
+                                <option value="WV">West Virginia</option>
+                                <option value="WI">Wisconsin</option>
+                                <option value="WY">Wyoming</option>
+                            </select>
                         </div>
                         <div className="col-md-2">
                         <label for="inputZip" className="form-label">Zip</label>
                         <input type="text" className="form-control" id="inputZip"/>
                         </div>
                         <div className="container mt-3">
-                            <label for="peopleCount" className="form-label">Maximum Number of People:</label>
-                            <input type="number" className="form-control" id="peopleCount" min="0" max="10" value="0"/>
-                            <div id="peopleDisplay" className="mt-2">0 out of 10</div>
+                            <label htmlFor="peopleCount" className="form-label">Maximum Number of People:</label>
+                            <input type="number" className="form-control" id="peopleCount" min="1" max="10" value={peopleCount} onChange={handlePeopleCountChange}/>
+                            <div id="peopleDisplay" className="mt-2">{peopleCount} out of 10</div>
                         </div>
                         <div className="container mt-3">
                             <label for="descriptionBox" className="form-label">Additional Information:</label>
@@ -72,8 +147,8 @@ const CreateEdit = () => {
                         </div> 
                         <div className="container mb-3">
                             <label for="customRange2" className="form-label">Level Of Expertise</label>
-                            <input type="range" className="form-range" min="0" max="5" id="customRange2"/>
-                            <div id="rangeValue">Beginner</div>
+                            <input type="range" className="form-range" min="0" max="3" id="customRange2" onChange={handleRangeChange}/>
+                            <div id="rangeValue">{expertiseLevel}</div>
                         </div> 
 
                         <div className="row mt-2">
