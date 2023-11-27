f008f6bda2a797161892ebe807c9adbb382686d3
swaugh64 <samawaugh@gmail.com>
2023-11-26 19:12:11 -0800

    Added static json server and updated tables

--- sports-matchup/src/Home.js
+++ sports-matchup/src/Home.js
@@ -1,21 +1,50 @@
 // Home.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-// import './App.css'
 import home_png from './images/home.png'
-
+import events from './event_info.json'
 const Home = () => {
     const location = useLocation();
+    const [sortField, setSortField] = useState(null);
+    const [sortDirection, setSortDirection] = useState('asc');
+
+    const [selectedSport, setSelectedSport] = useState('');
+
     useEffect(() => {
         switch (location.hash) {
           case '#discover-events':
             document.title = 'Discover Events - Sports Matchup';
             break;
-          // other cases...
           default:
             document.title = 'Home - Sports Matchup';
         }
       }, [location]);
+
+    const handleSort = (field) => {
+        let direction = 'asc';
+        if (sortField === field && sortDirection === 'asc') {
+            direction = 'desc';
+        }
+        setSortField(field);
+        setSortDirection(direction);
+    };
+
+    const sortedEvents = [...events].sort((a, b) => {
+        if (a[sortField] < b[sortField]) {
+            return sortDirection === 'asc' ? -1 : 1;
+        }
+        if (a[sortField] > b[sortField]) {
+            return sortDirection === 'asc' ? 1 : -1;
+        }
+        return 0;
+    });
+
+    const handleSportChange = (event) => {
+        setSelectedSport(event.target.value);
+    };
+
+    const filteredEvents = sortedEvents.filter(event => !selectedSport || event.sport === selectedSport);
+
 
     return (
         <main>
@@ -34,38 +63,44 @@ const Home = () => {
                         <div className="form-group row mt-1">
                             <label for="search" className="col-sm-1 col-form-label">Search:</label>
                             <div className="col-sm-10">
-                                <select className="form-control" id="sport">
-                                    <option value="" selected disabled>Select Sports...</option>
-                                    <option value="basketball">Basketball</option>
-                                    <option value="baseball">Baseball</option>
-                                    <option value="soccer">Soccer</option>
-                                    <option value="football">Football</option>
-                                    <option value="volleyball">Volleyball</option>
-                                    <option value="hockey">Hockey</option>
-                                    <option value="golf">Golf</option>
-                                    <option value="tennis">Tennis</option>
-                                    <option value="other-sport">Other</option>
+                                <select className="form-control" id="sport" value={selectedSport} onChange={handleSportChange}>
+                                    <option value="" selected>Select Sport...</option>
+                                    <option value="Basketball">Basketball</option>
+                                    <option value="Baseball">Baseball</option>
+                                    <option value="Soccer">Soccer</option>
+                                    <option value="Football">Football</option>
+                                    <option value="Volleyball">Volleyball</option>
+                                    <option value="Hockey">Hockey</option>
+                                    <option value="Golf">Golf</option>
+                                    <option value="Tennis">Tennis</option>
+                                    <option value="Other">Other</option>
                                 </select>
                             </div>
                         </div>
-                        {/* <div id="categoryModal" className="modal" tabindex="-1">
-                            <div className="modal-dialog modal-dialog-centered">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h2 className="modal-title">Add New Sport</h2>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
-                                    </div>
-                                    <div className="modal-body">
-                                        <label for="newCategory"></label>
-                                        <input type="text" className="form-control" id="newCategory" placeholder="New Category"/>
-                                        <button id="addCategory" className="btn btn-primary mt-2">Add Sport</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> */}
                     </form>
                     <h3 className="mt-4">Events in your Area:</h3>
-                    <div id="eventsFilter" className="mt-3"></div>
+                    <div id="eventsFilter" className="mt-3">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th onClick={() => handleSort('sport')}>Sport</th>
+                                    <th onClick={() => handleSort('location')}>Location</th>
+                                    <th onClick={() => handleSort('members')}>Members</th>
+                                    <th onClick={() => handleSort('title')}>Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filteredEvents.map(event => (
+                                    <tr key={event.id}>
+                                        <td>{event.sport}</td>
+                                        <td>{event.location}</td>
+                                        <td>{event.members}</td>
+                                        <td>{event.title}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </section>
         </main>
